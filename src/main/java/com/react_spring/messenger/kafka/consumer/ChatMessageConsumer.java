4c4ac27ae77b1bc97286e74ad124c22e87bc5f9d
Andrey <kandrei704@gmail.com>
2025-08-24 12:08:14 +0300

    redis notification management initial implementation

--- src/main/java/com/react_spring/messenger/kafka/consumer/ChatMessageConsumer.java
+++ src/main/java/com/react_spring/messenger/kafka/consumer/ChatMessageConsumer.java
@@ -1,15 +1,23 @@
 package com.react_spring.messenger.kafka.consumer;
 
 import com.react_spring.messenger.kafka.model.ChatMessage;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatMessageConsumer {
 
+    private final RedisTemplate<String, Integer> redisTemplate;
+
+    public ChatMessageConsumer(RedisTemplate<String, Integer> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @KafkaListener(topics = "chat-messages", groupId = "chat-group")
     public void consume(ChatMessage message) {
         System.out.println("Received message: " + message);
-        // TODO: persist to Postgres, update Redis, etc.
+        String key = "unread:" + message.getChatId() + ":" + message.getReceiverId(); //key: "unread:{message.getChatId()}:{message.getReaderId()}"
+        redisTemplate.opsForValue().increment(key);
     }
 }
--- src/main/java/com/react_spring/messenger/kafka/consumer/ChatReadConsumer.java
+++ src/main/java/com/react_spring/messenger/kafka/consumer/ChatReadConsumer.java
@@ -1,16 +1,24 @@
 package com.react_spring.messenger.kafka.consumer;
 
-import com.react_spring.messenger.kafka.model.ChatMessage;
 import com.react_spring.messenger.kafka.model.ChatRead;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatReadConsumer {
 
+    private final RedisTemplate<String, Integer> redisTemplate;
+
+    public ChatReadConsumer(RedisTemplate<String, Integer> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @KafkaListener(topics = "chat-read", groupId = "chat-group")
     public void consume(ChatRead message) {
-        System.out.println("red message: " + message);
-        // TODO: persist to Postgres, update Redis, etc.
+        System.out.println("read message: " + message);
+
+        String key = "unread:" + message.getChatId() + ":" + message.getReaderId(); //key: "unread:{message.getChatId()}:{message.getReaderId()}"
+        redisTemplate.opsForValue().decrement(key);
     }
 }
